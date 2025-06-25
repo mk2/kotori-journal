7f7c78c8aa185b64bb95c5822383122201c5d079
mk2 <mk2@mk2.red>
2025-06-25 16:35:39 +0900

    (fix) コンテナビルド時にclaudeをローカルインストールに切り替える

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,5 +6,6 @@ mise settings add idiomatic_version_file_enable_tools node
 mise install
 
 npm install -g @anthropic-ai/claude-code
+npx claude migrate-installer
 echo 'alias claude="~/.claude/local/claude"' >> ~/.bashrc
 echo 'export PATH=~/.claude/local:$PATH' >> ~/.bashrc
